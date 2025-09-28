a531f60b242bb122372989eb792b02adc0772833
cjlopez27 <carlosjlopezz27@csu.fullerton.edu>
2025-09-28 16:02:04 -0700

    Added a couple of files and changed configuration settings

--- My-Financial-Picture/vite.config.js
+++ My-Financial-Picture/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const isGhPages = !!process.env.GITHUB_PAGES
+const base = isGhPages ? '/My-Financial-Picture-CPSC-491/' : '/'
+
 export default defineConfig({
   plugins: [react()],
   test: {
